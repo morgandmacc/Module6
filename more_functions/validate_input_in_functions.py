5f2901ac561c826d3e7d62c00edb409cc754740e
morganmccarley <morgan.mccarley@gmail.com>
2020-09-29 19:11:14 -0500

    completed score_input functionality with full unit test coverage.

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -19,9 +19,18 @@ def score_input(test_name, test_score = 0, invalid_message = 'Invalid test score
     :return: formatted string with test name and test score.
     :rtype: String
     """
-    #print("{}: {}".format(test_name, test_score))
-    return "{}: {}".format(test_name, test_score)
+    try:
+        if 0 <= test_score <= 100:
+            #print("{}: {}".format(test_name, test_score))
+            return "{}: {}".format(test_name, test_score)
+        else:
+            return invalid_message
+    except TypeError:
+        raise TypeError
 
 
 if __name__ == '__main__':
-    score_input("test1", 45)
+    try:
+        print(score_input("test1", "45"))
+    except TypeError:
+        print("not a valid score!")
--- test_functions/test_validate_input_in_functions.py
+++ test_functions/test_validate_input_in_functions.py
@@ -7,19 +7,20 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("Python1!: 0", val_input.score_input("Python1!"))
 
     def test_score_input_test_score_valid(self):
-        self.assertEqual(True, False)
+        self.assertEqual("Python2!: 97", val_input.score_input("Python2!", 97))
 
     def test_score_input_test_score_below_range(self):
-        self.assertEqual(True, False)
+        self.assertEqual("Invalid test score, try again!", val_input.score_input("Python3!", -1))
 
     def test_score_input_test_score_above_range(self):
-        self.assertEqual(True, False)
+        self.assertEqual("Invalid test score, try again!", val_input.score_input("Python4!", 101))
 
     def test_test_score_non_numeric(self):
-        self.assertEqual(True, False)
+        with self.assertRaises(TypeError):
+            val_input.score_input("Python5", "fifty")
 
     def test_score_input_invalid_message(self):
-        self.assertEqual(True, False)
+        self.assertEqual("Not an acceptible score, try again!", val_input.score_input("Python6!", 101, "Not an acceptible score, try again!"))
 
 
 if __name__ == '__main__':
